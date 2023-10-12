65aa94f7ebb7b5e43e2c1a9bbe0c1830e8ed0e39
Alexandr <107064917+etrehappy@users.noreply.github.com>
2023-10-12 08:29:58 +0300

    base_task comleted + ignore + sfml

--- cmake_build/cmake_install.cmake
+++ cmake_build/cmake_install.cmake
@@ -32,6 +32,12 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("C:/Users/etreh/Documents/Programming/Otus/C++_Developer_2023_06/Basic/lesson_10/otus_cpp_basic_hw4/cmake_build/_deps/sfml-build/cmake_install.cmake")
+
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
